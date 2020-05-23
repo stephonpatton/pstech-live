e9cb6dda8f87b0222a9abe55be0648a0637ba0ac
stephonpatton <stephonpatton@icloud.com>
2020-05-23 17:17:15 -0400

    - Working on hosting issues still

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
           >
-            Editing....
+            Editing.... still.
           </a>
         </header>
       </div>
